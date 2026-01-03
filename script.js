55693d0056f9a0374ee720952f23530e5c86ea23
kprakash-07 <kprakash8924@gmail.com>
2026-01-03 19:15:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+        function calculateBMI()
+        {
+            let w=document.getElementById("weight").value;
+            let h=document.getElementById("Height").value;
+            let bmi=w/((h/100)**2);
+            bmi=bmi.toFixed(2);
+            document.getElementById("result").innerHTML=`Your BMI is : ${bmi}`;
+        }
+</script>
